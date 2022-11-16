58c2c86711663e48da6df0ac47cb6ebd3a595c95
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-16 21:07:24 +0900

    immer 속성 + useState의 함수형 업데이트 적용

--- src/App.js
+++ src/App.js
@@ -125,6 +125,125 @@
 // });
 
 // App 컴포넌트에 immer 적용하기
+// import React, {useRef, useCallback, useState} from 'react';
+// import produce from 'immer';
+
+// const App = () => {
+//   const nextId = useRef(1);
+//   const [form, setForm] = useState({name: '', username: ''});
+//   const [data, setData] = useState({
+//     array: [],
+//     uselessValue: null
+//   });
+
+//   // input 수정을 위한 함수
+//   const onChange = useCallback(
+//     e => {
+//       const {name, value} = e.target;
+//       setForm(
+//         produce(form, draft => {
+//           draft[name] = value;
+//         })
+//       );
+//       // setForm({
+//       //   ...form,
+//       //   [name]: [value]
+//       // });
+//     },
+//     [form]
+//   );
+
+//   // form 등록을 위한 함수
+//   const onSubmit = useCallback(
+//     e => {
+//       e.preventDefault();
+//       const info = {
+//         id: nextId.current,
+//         name: form.name,
+//         username: form.username
+//       };
+
+//       // array에 새 항목 등록
+//       setData(
+//         produce(data, draft => {
+//           draft.array.push(info);
+//         })
+//       );
+//       // setData({
+//       //   ...data,
+//       //   array: data.array.concat(info)
+//       // });
+
+//       // form 초기화
+//       setForm({
+//         name: '',
+//         username: ''
+//       });
+//       nextId.current += 1;
+//     },
+//     [data, form.name, form.username]
+//   );
+
+//   // 항목을 삭제하는 함수
+//   const onRemove = useCallback(
+//     id => {
+//       setData(
+//         produce(data, draft => {
+//           draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+//         })
+//       );
+//       // setData({
+//       //   ...data,
+//       //   array: data.array.filter(info => info.id !== id)
+//       // });
+//     },
+//     [data]
+//   );
+
+//   return (
+//     <>
+//       <form onSubmit={onSubmit}>
+//         <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
+//         <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
+//         <button type="submit">등록</button>
+//       </form>
+//       <>
+//         <ul>
+//           {data.array.map(info => (
+//             <li key={info.id} onClick={() => onRemove(info.id)}>
+//               {info.username} ({info.name})
+//             </li>
+//           ))}
+//         </ul>
+//       </>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// useState의 함수형 업데이트와 immer 함께 쓰기
+// 예시
+// const [number, setNumber] = useState(0);
+// // prevNumbers는 현재 number 값을 가리킵니다.
+// const onIncrease = useCallback(
+//   () => setNumber(prevNumber => prevNumber + 1),
+//   [],
+// );
+
+// immer에서 제공하는 produce 함수를 호출할 때, 첫 번째 파라미터가 함수 형태이면 업데이트 함수를 반환.
+// 예시
+// const update = produce(draft => {
+//   draft.value = 2;
+// });
+// const originalState = {
+//   value: 1,
+//   foo: 'bar',
+// };
+// const nextState = update(originalState);
+// console.log(nextState); // {value: 2, foo: 'bar'}
+
+// immer 속성 + useState의 함수형 업데이트 적용
 import React, {useRef, useCallback, useState} from 'react';
 import produce from 'immer';
 
@@ -141,14 +260,15 @@ const App = () => {
     e => {
       const {name, value} = e.target;
       setForm(
-        produce(form, draft => {
+        produce(draft => {
           draft[name] = value;
         })
       );
-      // setForm({
-      //   ...form,
-      //   [name]: [value]
-      // });
+      // setForm(
+      //   produce(form, draft => {
+      //     draft[name] = value;
+      //   })
+      // );
     },
     [form]
   );
@@ -165,14 +285,15 @@ const App = () => {
 
       // array에 새 항목 등록
       setData(
-        produce(data, draft => {
+        produce(draft => {
           draft.array.push(info);
         })
       );
-      // setData({
-      //   ...data,
-      //   array: data.array.concat(info)
-      // });
+      // setData(
+      //   produce(data, draft => {
+      //     draft.array.push(info);
+      //   })
+      // );
 
       // form 초기화
       setForm({
@@ -181,23 +302,26 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
+    // [data, form.name, form.username]
   );
 
   // 항목을 삭제하는 함수
   const onRemove = useCallback(
     id => {
       setData(
-        produce(data, draft => {
-          draft.array.splice(drafe.array.findIndex(info => info.id === id), 1);
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
         })
       );
-      // setData({
-      //   ...data,
-      //   array: data.array.filter(info => info.id !== id)
-      // });
+      // setData(
+      //   produce(data, draft => {
+      //     draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+      //   })
+      // );
     },
-    [data]
+    []
+    // [data]
   );
 
   return (
