3976b70fb84626342cb94f969189dc915e7a45e9
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-16 20:56:40 +0900

    App 컴포넌트에 immer 적용하기

--- src/App.js
+++ src/App.js
@@ -1,84 +1,84 @@
 // immer를 사용하지 않고 불변성 유지
-import React, {useRef, useCallback, useState} from 'react';
+// import React, {useRef, useCallback, useState} from 'react';
 
-const App = () => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({name: '', username: ''});
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null
-  });
+// const App = () => {
+//   const nextId = useRef(1);
+//   const [form, setForm] = useState({name: '', username: ''});
+//   const [data, setData] = useState({
+//     array: [],
+//     uselessValue: null
+//   });
 
-  // input 수정을 위한 함수
-  const onChange = useCallback(
-    e => {
-      const {name, value} = e.target;
-      setForm({
-        ...form,
-        [name]: [value]
-      });
-    },
-    [form]
-  );
+//   // input 수정을 위한 함수
+//   const onChange = useCallback(
+//     e => {
+//       const {name, value} = e.target;
+//       setForm({
+//         ...form,
+//         [name]: [value]
+//       });
+//     },
+//     [form]
+//   );
 
-  // form 등록을 위한 함수
-  const onSubmit = useCallback(
-    e => {
-      e.preventDefault();
-      const info = {
-        id: nextId.current,
-        name: form.name,
-        username: form.username
-      };
+//   // form 등록을 위한 함수
+//   const onSubmit = useCallback(
+//     e => {
+//       e.preventDefault();
+//       const info = {
+//         id: nextId.current,
+//         name: form.name,
+//         username: form.username
+//       };
 
-      // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+//       // array에 새 항목 등록
+//       setData({
+//         ...data,
+//         array: data.array.concat(info)
+//       });
 
-      // form 초기화
-      setForm({
-        name: '',
-        username: ''
-      });
-      nextId.current += 1;
-    },
-    [data, form.name, form.username]
-  );
+//       // form 초기화
+//       setForm({
+//         name: '',
+//         username: ''
+//       });
+//       nextId.current += 1;
+//     },
+//     [data, form.name, form.username]
+//   );
 
-  // 항목을 삭제하는 함수
-  const onRemove = useCallback(
-    id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
-    },
-    [data]
-  );
+//   // 항목을 삭제하는 함수
+//   const onRemove = useCallback(
+//     id => {
+//       setData({
+//         ...data,
+//         array: data.array.filter(info => info.id !== id)
+//       });
+//     },
+//     [data]
+//   );
 
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
-        <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
-        <button type="submit">등록</button>
-      </form>
-      <>
-        <ul>
-          {data.array.map(info => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username} ({info.name})
-            </li>
-          ))}
-        </ul>
-      </>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <form onSubmit={onSubmit}>
+//         <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
+//         <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
+//         <button type="submit">등록</button>
+//       </form>
+//       <>
+//         <ul>
+//           {data.array.map(info => (
+//             <li key={info.id} onClick={() => onRemove(info.id)}>
+//               {info.username} ({info.name})
+//             </li>
+//           ))}
+//         </ul>
+//       </>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
 
 // immer 사용법
 // 예시
@@ -123,3 +123,101 @@ export default App;
 //   // id = 1인 항목을 제거하기
 //   draft.splice(draft.findIndex(t => t.id === 1), 1);
 // });
+
+// App 컴포넌트에 immer 적용하기
+import React, {useRef, useCallback, useState} from 'react';
+import produce from 'immer';
+
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({name: '', username: ''});
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null
+  });
+
+  // input 수정을 위한 함수
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
+      // setForm({
+      //   ...form,
+      //   [name]: [value]
+      // });
+    },
+    [form]
+  );
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username
+      };
+
+      // array에 새 항목 등록
+      setData(
+        produce(data, draft => {
+          draft.array.push(info);
+        })
+      );
+      // setData({
+      //   ...data,
+      //   array: data.array.concat(info)
+      // });
+
+      // form 초기화
+      setForm({
+        name: '',
+        username: ''
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    id => {
+      setData(
+        produce(data, draft => {
+          draft.array.splice(drafe.array.findIndex(info => info.id === id), 1);
+        })
+      );
+      // setData({
+      //   ...data,
+      //   array: data.array.filter(info => info.id !== id)
+      // });
+    },
+    [data]
+  );
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
+        <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <>
+        <ul>
+          {data.array.map(info => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </>
+    </>
+  );
+};
+
+export default App;
